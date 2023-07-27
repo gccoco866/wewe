030d8afb868736a6ffca8ec9148f4e31fb8b3dd1
Brettley Davis <bttdavis@yahoo.com>
2023-07-27 13:09:47 -0500

    this is my first git

---
+++ math.py
@@ -0,0 +1,20 @@
+x = 4
+y = 5
+
+print('%g + %g = %g'%(x,y,x+y))
+
+print(3**2*3**4)
+
+for qwerty in range(0,3):
+    print('This is the %gth iteration' %qwerty)
+
+for t in range(0,5):
+    num = t**2
+    print(num)
+
+base = 8
+for i in range(0,10):
+    print('%g to the power of %g is %g' %(base,i,base**i))
+
+b = 4 < 5
+print(b)
---
+++ sliceback.py
@@ -0,0 +1,10 @@
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+backwards = letters[4::-1]
+print(backwards)
+
+print(letters[-8:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
+
